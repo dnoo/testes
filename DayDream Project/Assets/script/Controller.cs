9ef0e781844f7f427e5d643f5e92cb16e5171d08
dnoo <ndoagiuscuhkie@gmail.com>
2018-05-19 16:34:31 +0900

    コントローラーの位置とか取得
    GetContolloreXYZコントローラー座標
    GetTouchPosタッチパット座標
    GetTouchClickタッチパットクリック
    GetaAppClickアプリボタンクリック

---
+++ DayDream Project/Assets/script/Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+	void Update(){
+		Debug.LogFormat ("{0},{1},{2},{3}", GetContllorXYZ (), GetTouchPosXY (), GetTouchClick (), GetAppClick ());
+	}
+
+	Vector3 GetContllorXYZ(){
+		Vector3 _xyz = new Vector3(GvrController.Orientation.x, GvrController.Orientation.y, GvrController.Orientation.z);
+		return _xyz;
+	}
+	Vector2 GetTouchPosXY(){
+		Vector2 _xy = new Vector2 (GvrController.TouchPos.x,GvrController.TouchPos.y);
+		return _xy;
+	}
+
+	bool GetTouchClick(){
+		bool _touch = GvrController.ClickButton;
+		return _touch;
+	}
+
+	bool GetAppClick(){
+		bool _click = GvrController.AppButton;
+		return _click;
+	}
+}
